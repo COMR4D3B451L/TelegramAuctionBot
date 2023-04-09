44dacbf2f4bbdeb1bdf38e152011e3bca882b179
api <basilaburagheef@gmail.com>
2023-04-10 01:30:11 +0200

    init

---
+++ bot.py
@@ -0,0 +1,36 @@
+import os
+from telebot import TeleBot, types
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+bot = TeleBot(API_KEY)
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['start', 'ابدء', 'ابدي'])
+def send_welcome(message):
+    print(message.from_user.id)
+    print(message.from_user.username)
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(commands=['test'])
+def test(message):
+    photo = open('./tmp/56.png', 'rb')
+    bot.send_photo(message.chat.id, photo)
+    
+@bot.message_handler(commands=['test2'])
+def key(message):
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    itembtn1 = types.KeyboardButton('a')
+    itembtn2 = types.KeyboardButton('v')
+    itembtn3 = types.KeyboardButton('d')
+    markup.add(itembtn1, itembtn2, itembtn3)
+    bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+    
+    
+    
+bot.polling()
---
+++ models.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+import uuid, datetime
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String, unique=True, index=True)
+    TelegramUserId = Column(String)
+    is_seller = Column(Boolean, default=False)
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    images = Column(String)
+    initial_price = Column(Integer)
+    min_bid = Column(Integer)
+    buy_now_price = Column(Integer)
+    bids = Column(Integer)
+    created_on = Column(DateTime)
+    ends_at = Column(DateTime)
+    
+    owner_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    owner = relationship("User", back_populates="items")
+    
+
+class Bid(Base):
+    __tablename__ = "bids"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    amount = Column(Integer)
+    timestamp = Column(DateTime)
+    
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    user = relationship("User", back_populates="bids")
+    
+    item_id = Column(UUID(as_uuid=True), ForeignKey("items.id"))
+    item = relationship("Item", back_populates="bids")
+    
